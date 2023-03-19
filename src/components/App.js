e69177897b12e74570e2f5e9244ec927988447cb
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-03-19 13:45:21 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-function App() {
+const App = () => {
   return (
     <div>
-      <h1>Hello World!</h1>
-      <p>This is my first React app.</p>
+      <h1>Hello, World!</h1>
     </div>
   );
-}
+};
 
 export default App;
