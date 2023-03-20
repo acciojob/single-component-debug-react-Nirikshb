dd86bd9d53e0a768c7a6c3900f8d131e62cc39d2
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-03-20 14:18:11 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <h1>Hello, world!</h1>
+      <p>I can now create full-fledged React components and render it on the DOM node of my choice.</p>
     </div>
   );
 };
